9abe05620fe2d6b2381367875efa342c6ca6d417
LopezDev <aragonlopezjuan87@gmail.com>
2024-03-29 07:24:48 +0100

    logica del componente containertask

--- src/containers/containerTask.tsx
+++ src/containers/containerTask.tsx
@@ -1,20 +1,69 @@
 import { task } from "src/interfaces"
+import { Button } from '../components/button'
+import { Card } from '../components/card'
+import { ChangeEvent, FormEvent, useId } from "react"
 
-interface containerTask {
-  tasks: task[]
+type oldState = {
+  oldState: task[]
 }
 
-export const ContainerTask = ({ tasks }: containerTask): JSX.Element => {
+type setNewTask = {
+  arg: boolean | task
+}
+interface containerTask {
+  tasks: task[]
+  newTask: boolean | task[]
+  setNewTask: (task: setNewTask) => boolean | task[]
+  setTask: (oldState: oldState) => task[]
+}
+
+
+
+export const ContainerTask = ({ tasks, newTask, setNewTask, setTask }: containerTask): JSX.Element => {
+
+  const handleNewTask = () => {
+    setNewTask(true)
+  }
+
+  const id = useId()
+
+  const handleOnChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setNewTask({
+      task: e.target.value,
+      id: id
+    })
+  }
+
+  const handleOnSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    setTask(oldState => [...oldState, newTask])
+    setNewTask(false)
+  }
   return (
-    <div className='grid gap-4'>
+    <div className='grid gap-4 p-8 bg-slate-100 max-w-fit'>
       {
         tasks.map((task: task) => {
           return (
-            <article key={task.id} className="w-64  min-h-32 p-6 border rounded-xl shadow-md">
+            <Card key={task.id} >
               <p className="font-base">{task.task}</p>
-            </article>
+            </Card>
           )
         })
+      }
+      {
+        !newTask ?
+          (<Button type='button' onClick={() => handleNewTask()}>Nueva nota</Button>) :
+          (<Card>
+            <form className="flex flex-col gap-4" onSubmit={(e) => handleOnSubmit(e)}>
+              <label>Nueva nota</label>
+              <textarea className="p-2" onChange={(e) => handleOnChange(e)} placeholder="Escribe tu nueva tarea"></textarea>
+              <div className="grid">
+                <Button type='submit'>Agregar nueva nota</Button>
+              </div>
+            </form>
+
+          </Card>)
+
       }
     </div>
   )
