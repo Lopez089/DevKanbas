a29954e5db641d37e246de601c18178ad772f37c
LopezDev <aragonlopezjuan87@gmail.com>
2024-03-29 09:15:48 +0100

    crear el elect para selecionar el estado de la notas

--- src/containers/containerTask.tsx
+++ src/containers/containerTask.tsx
@@ -3,42 +3,42 @@ import { Button } from '../components/button'
 import { Card } from '../components/card'
 import { ChangeEvent, FormEvent, useId } from "react"
 
-type oldState = {
-  oldState: task[]
-}
-
-type setNewTask = {
-  arg: boolean | task
-}
 interface containerTask {
   tasks: task[]
-  newTask: boolean | task[]
-  setNewTask: (task: setNewTask) => boolean | task[]
-  setTask: (oldState: oldState) => task[]
+  newTask: undefined | task
+  setNewTask: (arg0: task | undefined) => void
+  setTask: (oldState: task[]) => void
 }
 
-
-
 export const ContainerTask = ({ tasks, newTask, setNewTask, setTask }: containerTask): JSX.Element => {
-
-  const handleNewTask = () => {
-    setNewTask(true)
-  }
-
   const id = useId()
 
-  const handleOnChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+  const handleNewTask = () => {
     setNewTask({
-      task: e.target.value,
+      id: '',
+      task: '',
+      state: 'not stated'
+    })
+  }
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    if (!newTask) return;
+
+    setNewTask({
+      ...newTask,
+      [e.target.name]: e.target.value,
       id: id
     })
   }
 
   const handleOnSubmit = (e: FormEvent) => {
     e.preventDefault()
-    setTask(oldState => [...oldState, newTask])
-    setNewTask(false)
+    if (newTask) {
+      setTask([...tasks, newTask])
+      setNewTask(undefined)
+    }
   }
+
   return (
     <div className='grid gap-4 p-8 bg-slate-100 max-w-fit'>
       {
@@ -56,7 +56,13 @@ export const ContainerTask = ({ tasks, newTask, setNewTask, setTask }: container
           (<Card>
             <form className="flex flex-col gap-4" onSubmit={(e) => handleOnSubmit(e)}>
               <label>Nueva nota</label>
-              <textarea className="p-2" onChange={(e) => handleOnChange(e)} placeholder="Escribe tu nueva tarea"></textarea>
+              <textarea className="p-2 rounded-lg" name='task' onChange={(e) => handleOnChange(e)} placeholder="Escribe tu nueva tarea"></textarea>
+              <select name='state' className="p-2 rounded-lg" onChange={(e) => { handleOnChange(e) }}>
+                <option value="not stated" key="not stated">Not Stated</option>
+                <option value="in progress" key="in progress">In Progress</option>
+                <option value="done" key="done">Done</option>
+                <option value="archived" key="archived">Archived</option>
+              </select>
               <div className="grid">
                 <Button type='submit'>Agregar nueva nota</Button>
               </div>
